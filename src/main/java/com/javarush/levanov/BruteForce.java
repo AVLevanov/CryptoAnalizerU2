0ef4624120f9f8d241893ca2575f7ae1a15184c6
Alexander Levanov <alevanov@web3tech.ru>
2026-05-05 07:41:44 +0300

    Добавлены различные комбинации знаков препинания/пробелов/переводов строки до/после ключевого слова при переборе

--- src/main/java/com/javarush/levanov/BruteForce.java
+++ src/main/java/com/javarush/levanov/BruteForce.java
@@ -4,36 +4,37 @@ import java.util.*;
 
 public class BruteForce {
     private static final String[] KEY_WORDS = {"и", "в", "на", "не", "я", "он", "она", "человк", "время", "год", "был"};
+    private static final String[] PRECEDING_CHARACTERS = {" ", "\n"};
+    private static final String[] NEXT_CHARACTERS = {" ", ".", ",", ";", ":", "\"", "!", "?"};
 
     // возвращаем расшифрованный текст по значению найденного ключа
     public String decryptByBruteForce(String encryptedText, int alphabetLength) {
         Cipher cipher = new Cipher();
-        int foundedKey = findKey(encryptedText, alphabetLength);
+        int foundedKey = findKey(encryptedText, alphabetLength, cipher);
         return cipher.decrypt(encryptedText, foundedKey);
     }
 
     // перебиреаем все ключи, и возвращаем тот, при котором больше всего повторений ключевых слов
-    private int findKey(String encryptedText, int alphabetLength) {
-        Cipher cipher = new Cipher();
-
-        Map<Integer, Integer> countersAndKeys = new HashMap<>();
+    private int findKey(String encryptedText, int alphabetLength, Cipher cipher) {
+        Map<Integer, Integer> keysAndCounters = new HashMap<>();
         for (int key = 1; key <= alphabetLength; key++) {
             String decryptedText = cipher.decrypt(encryptedText, key);
             int counter = keyWordsCounter(decryptedText);
-            countersAndKeys.put(key, counter);
+            keysAndCounters.put(key, counter);
         }
-//        System.out.println(countersAndKeys);
-        Iterator<Map.Entry<Integer, Integer>> iterator = countersAndKeys.entrySet().iterator();
+        System.out.println(keysAndCounters);
 
-        int maxValue = 0;
+        Iterator<Map.Entry<Integer, Integer>> iterator = keysAndCounters.entrySet().iterator();
+        int maxCounterValue = 0;
         int key = 0;
         while (iterator.hasNext()) {
             Map.Entry<Integer, Integer> nextEntry = iterator.next();
-            if (nextEntry.getValue() > maxValue) {
-                maxValue = nextEntry.getValue();
+            if (nextEntry.getValue() > maxCounterValue) {
+                maxCounterValue = nextEntry.getValue();
                 key = nextEntry.getKey();
             }
         }
+        System.out.println("key=" + key);
         return key;
     }
 
@@ -46,15 +47,17 @@ public class BruteForce {
         return keyWordCounter;
     }
 
-    // возвращает чисто повторений слова в тексте
+    // перебираем варианты обрамления слова пробелами и знаками препинания и возвращаем число повторений слова в тексте
     private int wordCounter(String text, String word) {
-        String[] wordCases = {" " + word + " ", "\n" + word + " ", " " + word + ".", " " + word + ","};
         int wordCounter = 0;
-        for (String wordCase : wordCases) {
-            int pointer = 0;
-            while (text.toLowerCase().indexOf(wordCase, pointer) != -1) {
-                wordCounter++;
-                pointer = text.toLowerCase().indexOf(wordCase, pointer) + wordCase.length();
+        for (String precedingCharacter : PRECEDING_CHARACTERS) {
+            for (String nextCharacter : NEXT_CHARACTERS) {
+                int pointer = 0;
+                String wordCase = precedingCharacter + word + nextCharacter;
+                while (text.toLowerCase().indexOf(wordCase, pointer) != -1) {
+                    wordCounter++;
+                    pointer = text.toLowerCase().indexOf(wordCase, pointer) + wordCase.length();
+                }
             }
         }
         return wordCounter;
